b5b82afd9c0a558f5541d4a7aa474dbdebf67f36
joonghyun <joongh@ewhain.net>
2023-06-11 02:35:03 +0900

    #2 feat: 홈 화면 조회 API

---
+++ src/main/java/com/umc/demo/user/dto/HomeRes.java
@@ -0,0 +1,14 @@
+package com.umc.demo.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class HomeRes {
+    private String myStatus;
+}
